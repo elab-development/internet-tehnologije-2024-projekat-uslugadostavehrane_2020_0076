aed30b26b8e058cec941ae36d854282bf485d020
cofiv <filip_vukovic@outlook.com>
2024-12-12 23:31:27 +0100

    kreirane migracije za model user

---
+++ domacilaravel/database/migrations/2024_12_12_223047_add_columns_to_users_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('ime')->after('id');
+            $table->string('prezime')->after('ime');
+            $table->string('adresa')->nullable()->after('email');
+            $table->string('telefon')->nullable()->after('adresa');
+            $table->decimal('latitude', 10, 8)->nullable()->after('telefon');
+            $table->decimal('longitude', 11, 8)->nullable()->after('latitude');
+            $table->string('uloga')->default('user')->after('longitude');  
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn([
+                'ime',
+                'prezime',
+                'adresa',
+                'telefon',
+                'latitude',
+                'longitude',
+                'uloga'
+            ]);
+        });
+    }
+};
