5591b5d18181acf8884fe10f520af1db1cb08346
cofiv <filip_vukovic@outlook.com>
2024-12-12 23:32:51 +0100

    migracija za kreiranje tabele restorana

---
+++ domacilaravel/database/migrations/2024_12_12_223208_create_restaurants_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('restaurants', function (Blueprint $table) {
+            $table->id();
+            $table->string('naziv');
+            $table->string('adresa');
+            $table->string('telefon')->nullable();
+            $table->text('opis')->nullable();
+            $table->decimal('latitude', 10, 8)->nullable();
+            $table->decimal('longitude', 11, 8)->nullable();
+            $table->string('tip_hrane')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('restaurants');
+    }
+};
