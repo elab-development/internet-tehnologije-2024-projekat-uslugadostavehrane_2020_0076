5818917bf1c7ffca3e9abc8aef055cae6aad0e1d
cofiv <filip_vukovic@outlook.com>
2024-12-19 23:47:50 +0100

    dodate rute

--- domacireact/src/App.js
+++ domacireact/src/App.js
@@ -1,12 +1,18 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import LandingPage from './components/LandingPage/LandingPage';
+ 
 
 function App() {
   return (
-    <div className="App">
-      <LandingPage></LandingPage>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
